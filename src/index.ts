b71ff5e5a91964e7150c86fe79bdd6f0ab683c52
danielliew <danieliewjh@gmail.com>
2021-07-12 14:07:39 +0800

    CU-a62gk0 - lorem ipsum

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send(`<h1>${HEALTHY}</h1>`));
+app.get("/", (req, res) => res.send(`<h1>${HEALTHY}</h1><p>Lorem ipsum</p>`));
 app.use("/todos", todos);
 
 export default app;
